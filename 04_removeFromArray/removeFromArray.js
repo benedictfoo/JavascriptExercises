93d9128741da91d3c4ba482585da07603faeed7a
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-01 14:54:18 +0800

    Pass all js exercises test 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...elements) {
+  const arrResult = arr.filter((item) => {
+    let elementsIndex = 0;
+    while (elementsIndex < elements.length) {
+      if (elements[elementsIndex] === item) {
+        elementsIndex = 9999;
+        return false;
+      } else {
+        elementsIndex++;
+      }
+    }
+    return true;
+  });
+  console.log(arrResult);
+  return arrResult;
 };
 
 // Do not edit below this line
