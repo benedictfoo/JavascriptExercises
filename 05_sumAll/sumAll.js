8c4a41e3a71f6759f3a9ad1288f54190ed768dfb
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-01 15:05:35 +0800

    Pass all js exercise 05 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return "ERROR";
+  } else {
+    let numStart = Math.min(num1, num2);
+    let numEnd = Math.max(num1, num2);
+    let total = 0;
+    while (numStart <= numEnd) {
+      total += numStart;
+      numStart++;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
