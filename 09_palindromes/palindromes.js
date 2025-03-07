fc93920b875eaf1bda6017a41f94aed42168c1bf
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-07 16:58:59 +0800

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const punctuationAndSpaces = /[\.\s,?!]/g;
+  const filteredString = string.replace(punctuationAndSpaces, "").toLowerCase();
+  let start = 0;
+  let end = filteredString.length - 1;
+  while (start < end) {
+    if (filteredString[start] !== filteredString[end]) {
+      return false;
+    } else {
+      start++;
+      end--;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
