138be3dfcc1e42ae28b0c47b0c71dcccc28633e9
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-07 17:18:38 +0800

    Fibonacci solve with recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let correctedInput = +num;
+  if (!Number.isInteger(correctedInput) || correctedInput < 0) {
+    return "OOPS";
+  }
+  if (correctedInput === 0) {
+    return 0;
+  } else if (correctedInput === 1 || correctedInput === 2) {
+    return 1;
+  } else {
+    return fibonacci(correctedInput - 2) + fibonacci(correctedInput - 1);
+  }
 };
 
 // Do not edit below this line
