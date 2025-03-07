e5e56198b1942230dbda4e3729604bbb2a666092
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-07 16:41:42 +0800

    Finish calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else if (arr.length === 1) {
+    return arr[0];
+  } else {
+    return arr.reduce((a, b) => a + b, 0);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  let timesToMultiply = pow - 1;
+  let result = num;
+  while (timesToMultiply) {
+    result *= num;
+    timesToMultiply--;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+  else {
+    let toMultiply = num - 1;
+    let result = num;
+    while (toMultiply > 1) {
+      result *= toMultiply;
+      toMultiply--;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
